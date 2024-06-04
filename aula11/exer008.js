8dbcc35fbbdc0086b34085b3d11a77b55773f465
Flaviana Fernandes <flavyolliveira@gmail.com>
2024-06-03 22:38:52 -0300

    criei os exercícios 8, 9, 10 e 11

---
+++ aula11/exer008.js
@@ -0,0 +1,10 @@
+//console.log("o console funcionou corretamente!")
+
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO(A)!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
